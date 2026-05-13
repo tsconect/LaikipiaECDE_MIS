6bc261ef377bff99944a78f0d7971a7b7e0b99d2
kigenchesire <ekchesire@kabarak.ac.ke>
2026-05-13 08:14:59 +0300

    ch

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -19,6 +19,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.min.js"></script>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+     {{-- j query 3.6 --}}
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
  @vite(['resources/css/admin.css', 'resources/js/app.js'])
  @stack('css')
 </head>
